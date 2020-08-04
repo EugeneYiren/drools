9b1807a7b681411d4b19023e9a67f175285008ff
eugenetan2015 <eugene.tan.2015@sis.smu.edu.sg>
2020-08-04 12:07:53 +0800

    init commit springboot app with dependencies

---
+++ droolsEngine/src/main/java/com/droolsUIEngine/droolsUIEngine/DroolsUiEngineApplication.java
@@ -0,0 +1,13 @@
+package com.droolsUIEngine.droolsUIEngine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DroolsUiEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DroolsUiEngineApplication.class, args);
+	}
+
+}
---
+++ droolsEngine/src/test/java/com/droolsUIEngine/droolsUIEngine/DroolsUiEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.droolsUIEngine.droolsUIEngine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DroolsUiEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
