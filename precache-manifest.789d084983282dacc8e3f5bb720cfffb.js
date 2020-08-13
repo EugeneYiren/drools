cc9e9e8a7a98a734a394b1d0213d891199bf5047
eugenetan2015 <eugene.tan.2015@sis.smu.edu.sg>
2020-08-13 11:38:27 +0800

    Updates

---
+++ precache-manifest.789d084983282dacc8e3f5bb720cfffb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14660fa16f2ad9184dbcfd3a5f39a9fb",
+    "url": "/drools/index.html"
+  },
+  {
+    "revision": "d6a564df434c526e72cc",
+    "url": "/drools/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "475b8afdd238080b3801",
+    "url": "/drools/static/js/2.0ad5225e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/drools/static/js/2.0ad5225e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6a564df434c526e72cc",
+    "url": "/drools/static/js/main.fca40749.chunk.js"
+  },
+  {
+    "revision": "5fef838461ebb2e237ea",
+    "url": "/drools/static/js/runtime-main.27548785.js"
+  }
+]);
