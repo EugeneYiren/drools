7bba1d09f3247d68347580f17154b7400b3378ce
eugenetan2015 <eugene.tan.2015@sis.smu.edu.sg>
2020-08-25 17:58:09 +0800

    added deployment stuff for both droolsapp and droolsEngine

--- droolsEngine/src/main/java/com/droolsUIEngine/droolsUIEngine/DroolsUiEngineApplication.java
+++ droolsEngine/src/main/java/com/droolsUIEngine/droolsUIEngine/DroolsUiEngineApplication.java
@@ -4,10 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.kie.api.KieServices;
 import org.kie.api.runtime.KieContainer;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class DroolsUiEngineApplication {
+public class DroolsUiEngineApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DroolsUiEngineApplication.class, args);
---
+++ droolsapp/target/droolsapp/precache-manifest.e1db1845c545adcdb462d7a8b923dbb2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44e9640d7d44816026e63e4741a98394",
+    "url": "http://localhost:8080/droolsapp/index.html"
+  },
+  {
+    "revision": "0299e0e34e3247d9faef",
+    "url": "http://localhost:8080/droolsapp/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "c040938741d691157c81",
+    "url": "http://localhost:8080/droolsapp/static/js/2.44c77f9c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "http://localhost:8080/droolsapp/static/js/2.44c77f9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0299e0e34e3247d9faef",
+    "url": "http://localhost:8080/droolsapp/static/js/main.f862a786.chunk.js"
+  },
+  {
+    "revision": "59a0317888e583c0e3e0",
+    "url": "http://localhost:8080/droolsapp/static/js/runtime-main.d962ad40.js"
+  }
+]);
