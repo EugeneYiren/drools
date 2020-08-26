9f8f46474a7701c459efeaba140ecf7ad8c20759
Eugene Yiren <eugenetyr94@gmail.com>
2020-08-26 15:21:27 +0800

    Try hashrouter for deployment

--- droolsapp/src/index.js
+++ droolsapp/src/index.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import './index.css'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter basename={process.env.REACT_APP_ROUTER_BASE || ''}>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
 )
